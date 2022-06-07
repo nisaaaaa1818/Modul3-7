1b4e66bc9d010196ff10e399a1ee968e7d135e56
Nisaaaaa1818 <18anisaazzahra@gmail.com>
2022-06-07 08:38:02 +0700

    Komit Pertama

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('container')
+    <h1> Halaman About </h1>
+    <h3> {{ $nama }} </h3>
+    <p>{{$TTL}}</p>
+    <p> {{ $email }} </p>
+    <p>{{$ig}}</p>
+    <img src="images/{{ $gambar }}" alt="{{ $nama }}" width="200px">
+@endsection
